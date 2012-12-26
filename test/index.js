49317ef38dbc1d367f3234333c7a37ef02f56132
Enrico Marino <enrico.marino@email.com>
2012-12-26 13:49:11 +0100

    added test

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var delegates = require('viewcollection');
+var Emitter = require('model');
+
+(function(){
+  var el = document.getElementById('list-one');
+  var collection = new Collection();
+  var view = ViewCollection(el, collection);
+  
+}());
